5c235fea1a0bc4dd08facec9e1c37b8b18c9be1a
“leytoncristian96” <leytoncristian96@gmail.com>
2023-12-26 23:31:37 -0300

    Configuracion Inicial

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,9 +5,9 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
                 @livewire('tareas')
             </div>
         </div>
